ee2244601ab8a9eaded14830e8ec46e01093130f
H.GOO <rngus3436@g.kmou.ac.kr>
2024-08-28 16:21:50 +0900

    refactor: 단행 return문 return 키워드 생략, if-let문 -> guard-let문 수정

--- CodeStarterCamp_Week2/step3.swift
+++ CodeStarterCamp_Week2/step3.swift
@@ -15,14 +15,15 @@ func recordLottoRounds(_ lottoNumbers: Set<Int>) {
 }
 
 func getCurrentRound() -> Int {
-    return lottoRoundDictionary.keys.count + 1
+    lottoRoundDictionary.keys.count + 1
 }
 
 func printLottoNumbers(round: Int) {
-    if let lottoNumbers: [Int] = lottoRoundDictionary[round] {
-        let numbersJoined: String = lottoNumbers.map{ String($0) }.joined(separator: ", ")
-        print("\(round)회차의 로또 당첨 번호는 \(numbersJoined) 입니다.")
-    } else {
+    guard let lottoNumbers: [Int] = lottoRoundDictionary[round] else {
         print("\(round)회차의 로또 당첨 번호를 찾을 수 없습니다.")
+        return
     }
+
+    let numbersJoined: String = lottoNumbers.map{ String($0) }.joined(separator: ", ")
+    print("\(round)회차의 로또 당첨 번호는 \(numbersJoined) 입니다.")
 }
